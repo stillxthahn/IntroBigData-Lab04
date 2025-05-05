cb0916c9e4506f5c16dfe2f938364ca49f4294da
FIXED-TERM Luong Xuan Thanh (MS/EDS31-XC) <fixed-term.Thanh.LuongXuan@vn.bosch.com>
2025-05-05 10:12:37 +0700

    update: kafka boostrap server

--- src/Load/22127293.py
+++ src/Load/22127293.py
@@ -6,9 +6,10 @@ spark = SparkSession.builder \
     .appName("BTC Price Streaming") \
     .getOrCreate()
 
+# -------- THANH: CHANGE BOOSTRAP SERVER FROM 127.0.0.1 to container host name
 df = spark.readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "127.0.0.1:9092") \
+    .option("kafka.bootstrap.servers", "kafka-broker:9092") \
     .option("subscribe", "btc-price-zscore") \
     .option("startingOffsets", "latest") \
     .load()
