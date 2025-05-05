37c595553287d04bc2f53b0b2c5dbafa74bdaaf3
FIXED-TERM Luong Xuan Thanh (MS/EDS31-XC) <fixed-term.Thanh.LuongXuan@vn.bosch.com>
2025-05-05 10:08:22 +0700

    change spark-master env in spark-worker

--- src/Transform/transform_spark_submit.bat
+++ src/Transform/transform_spark_submit.bat
@@ -9,8 +9,4 @@ if "%~1"=="" (
 set SCRIPT_NAME=%1
 
 echo ---Submitting spark job: %SCRIPT_NAME%
-docker exec spark-worker /opt/bitnami/spark/bin/spark-submit ^
-  --master spark://master:7077 ^
-  --deploy-mode client ^
-  --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 ^
-  /opt/spark-scripts-transform/%SCRIPT_NAME%
+docker exec spark-worker /opt/bitnami/spark/bin/spark-submit --master spark://spark-master:7077 --deploy-mode client --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 /opt/spark-scripts-transform/%SCRIPT_NAME%
